270a716d807d882bc1b1f866123f00b8bec29ca5
Nikita Sarah Putri <103813361+Sarahputri15@users.noreply.github.com>
2022-11-03 20:53:55 +0800

    visited

--- resources/views/Keuangan/partials/navbar3.blade.php
+++ resources/views/Keuangan/partials/navbar3.blade.php
@@ -7,7 +7,7 @@
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item dropdown ">
-            <a class="nav-link " href="{{ url('/Home/Pembayarankeu1') }}" >Persiapan Kontrak</a>
+            <a class="nav-link" href="{{ url('/Home/Pembayarankeu1') }}" >Persiapan Kontrak</a>
           </li>
         </ul>
       </div>
